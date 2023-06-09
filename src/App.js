a3aa26ed64943337be9a209d17b307de75caef1b
Aswathy <116071489+AswathyMS96@users.noreply.github.com>
2023-06-09 14:26:47 +0400

    App.js
    upadate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Footer from './components/Footer';
+import DailyStats from './components/DailyStat';
+import DevSection from './components/DevSection';
+import SocialMediaPromotion from './components/SocialMediaPromotion';
+import { BlogSection } from './components/BlogSection';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='home'>
+      <Header />
+      <Hero />
+      <DailyStats />
+      <DevSection />
+      <SocialMediaPromotion />
+      <BlogSection />
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
